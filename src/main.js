40eeb786feea1effd8b26be35479c2d7162f4be1
Belen Romero <romero.abelen@gmail.com>
2022-07-02 13:13:01 -0300

    descomentado

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 const app = require("./server");
-const args = require("../yargs/yargs");
+// const args = require("../yargs/yargs");
 
 const PORT = process.env.PORT; //Puerto desde yargs
 
 const server = app.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
-});
-
-app.get("/", (req, res) => {
-  res.render("home-login");
 });
 
 server.on("error", (error) => {
--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ dotenv.config();
 const compression = require("compression");
 app.use(compression());
 
-// const session = require("express-session");
-// const mongoStore = require("connect-mongo");
+const session = require("express-session");
+const mongoStore = require("connect-mongo");
 
-// const passport = require("./utils/passport.util");
-// const LocalStrategy = require("passport-local").Strategy;
+const passport = require("./utils/passport.util");
+const LocalStrategy = require("passport-local").Strategy;
 
 //EJS
 app.set("view engine", "ejs");
@@ -22,56 +22,56 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 /******************************SESSION***********************************/
-//al objeto de config de session le puedo dar un tiempo de expiracion a traves de cookies
-// app.use(
-//   session({
-//     store: mongoStore.create({
-//       mongoUrl:
-//         "mongodb+srv://belen:chiste@clustercoder.cqdn9.mongodb.net/sesiones",
-//       options: {
-//         userNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       },
-//     }),
-//     secret: process.env.SECRET,
-//     resave: true,
-//     saveUninitialized: true,
-//     cookie: {
-//       maxAge: Number(process.env.EXPIRE),
-//     },
-//     rolling: true,
-//   })
-// );
+// al objeto de config de session le puedo dar un tiempo de expiracion a traves de cookies
+app.use(
+  session({
+    store: mongoStore.create({
+      mongoUrl:
+        "mongodb+srv://belen:chiste@clustercoder.cqdn9.mongodb.net/sesiones",
+      options: {
+        userNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    }),
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: Number(process.env.EXPIRE),
+    },
+    rolling: true,
+  })
+);
 
 /******************************PASSPORT***********************************/
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// // ----Config router PRODUCTOS, CARRIO, CARRITO+PRODUCTOS
-// const routerCart = require("./routes/cartRoutesMongoDB.js");
-// const routerProducts = require("./routes/productRoutesMongoDB.js");
-// const routerCartProducts = require("./routes/cartProductsRoutesMongoDB.js");
-// // const routerUser = require("./routes/user");
-// const authRouter = require("./routes/auth.router");
-// const homeRouter = require("./routes/home");
+// ----Config router PRODUCTOS, CARRIO, CARRITO+PRODUCTOS
+const routerCart = require("./routes/cartRoutesMongoDB.js");
+const routerProducts = require("./routes/productRoutesMongoDB.js");
+const routerCartProducts = require("./routes/cartProductsRoutesMongoDB.js");
+// const routerUser = require("./routes/user");
+const authRouter = require("./routes/auth.router");
+const homeRouter = require("./routes/home");
 
-// //desafio 14***************************
-// const infoRouter = require("./routes/infoRouter.js");
-// const random = require("./routes/random.js");
+//desafio 14***************************
+const infoRouter = require("./routes/infoRouter.js");
+const random = require("./routes/random.js");
 
-// app.use("/api/products", routerProducts);
-// app.use("/api/cart", routerCart);
-// app.use("/api/cart/products", routerCartProducts);
-// // app.use("/api/user", routerUser);
-// app.use("/api/user", authRouter);
-// app.use("/", homeRouter);
+app.use("/api/products", routerProducts);
+app.use("/api/cart", routerCart);
+app.use("/api/cart/products", routerCartProducts);
+// app.use("/api/user", routerUser);
+app.use("/api/user", authRouter);
+app.use("/", homeRouter);
 
-// //desafio 14***********************
-// app.use("/info", infoRouter);
-// app.use("/api/random", random);
+//desafio 14***********************
+app.use("/info", infoRouter);
+app.use("/api/random", random);
 
-// app.get("/", (req, res) => {
-//   res.render("home-login");
-// });
+app.get("/", (req, res) => {
+  res.render("home-login");
+});
 
 module.exports = app;
