ce1f009f262e54f70e43315f687aa50e6bc5f27d
Belen Romero <romero.abelen@gmail.com>
2022-07-02 13:39:21 -0300

    modificacion de yarg por PORT en .env

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const app = require("./server");
-const args = require("../yargs/yargs");
+// const args = require("../yargs/yargs");
+// const PORT = args.port || 8080; //Puerto desde yargs
 
-const PORT = args.port || 8080; //Puerto desde yargs
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
